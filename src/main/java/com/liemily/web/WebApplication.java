43d605005aa5acd60ed3040511be5be4a4aa7795
emily-li <emily.li@live.co.uk>
2017-07-23 21:30:33 +0100

    Creating Spring Boot application to easily trigger database setup

---
+++ src/main/java/com/liemily/web/WebApplication.java
@@ -0,0 +1,14 @@
+package com.liemily.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Emily Li on 23/07/2017.
+ */
+@SpringBootApplication
+public class WebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
+}
