b0ccde0c9ee3e89d83b1497311393d289dda3a08
emily-li <emily.li@live.co.uk>
2017-07-23 23:41:34 +0100

    Refactoring StockRepositoryTest to use StockService

---
+++ src/main/java/com/liemily/springdatajpaexample/stock/StockService.java
@@ -0,0 +1,55 @@
+package com.liemily.springdatajpaexample.stock;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Created by Emily Li on 23/07/2017.
+ */
+@Service
+public class StockService {
+    private StockRepository stockRepository;
+
+    @Autowired
+    public StockService(StockRepository stockRepository) {
+        this.stockRepository = stockRepository;
+    }
+
+    @Transactional
+    public Stock findOne(String stockSymbol) {
+        return stockRepository.findOne(stockSymbol);
+    }
+
+    @Transactional
+    public List<Stock> findAll() {
+        List<Stock> stocks = new ArrayList<>();
+        Iterator<Stock> stockIterator = stockRepository.findAll().iterator();
+        stockIterator.forEachRemaining(stocks::add);
+        return stocks;
+    }
+
+    @Transactional
+    public Stock save(Stock stock) {
+        return stockRepository.save(stock);
+    }
+
+    @Transactional
+    public List<Stock> save(Iterable<Stock> stocks) {
+        return stockRepository.save(stocks);
+    }
+
+    @Transactional
+    public void delete(String stockSymbol) {
+        stockRepository.delete(stockSymbol);
+    }
+
+    @Transactional
+    public void delete(Iterable<Stock> stocks) {
+        stockRepository.delete(stocks);
+    }
+}
