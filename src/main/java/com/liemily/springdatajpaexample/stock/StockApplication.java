174f46f4fc4b9ce0c3a4848495c7695acac64cd2
emily-li <emily.li@live.co.uk>
2017-07-23 22:50:35 +0100

    Creating Spring Boot application

---
+++ src/main/java/com/liemily/springdatajpaexample/stock/StockApplication.java
@@ -0,0 +1,14 @@
+package com.liemily.springdatajpaexample.stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Emily Li on 23/07/2017.
+ */
+@SpringBootApplication
+public class StockApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StockApplication.class, args);
+    }
+}
