7f14f24d8cb2b71e0dd6740308ec92570d8d9a22
emily-li <emily.li@live.co.uk>
2017-07-23 22:52:17 +0100

    Correcting database SQL so decimal stores 2 DP

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,6 +2,6 @@ DROP TABLE IF EXISTS `stock`;
 
 CREATE TABLE IF NOT EXISTS `stock` (
     `symbol` VARCHAR(50)    NOT NULL PRIMARY KEY,
-    `value`  DECIMAL        NOT NULL,
+    `value`  DECIMAL(13,2)  NOT NULL,
     `volume` INT            NOT NULL
 );
